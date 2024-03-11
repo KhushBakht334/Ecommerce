1ff73c54b8be474ad5d7545775fd86244417c6a1
khushbakht334 <khush.bakht334@gmail.com>
2024-03-11 13:25:18 +0500

    m

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Admin from './Pages/Admin/Admin'
+import { BrowserRouter } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
+    <BrowserRouter>
     <Navbar/>
     <Admin/>
+    </BrowserRouter>
     </>
   )
 }
