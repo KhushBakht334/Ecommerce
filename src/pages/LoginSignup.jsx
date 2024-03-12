d3a3bf277cc3a37bfba1c5fed78a41d47003c79b
khushbakht334 <khush.bakht334@gmail.com>
2024-03-12 20:52:18 +0500

    login

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -3,20 +3,34 @@ import './CSS/LoginSignup.css'
 
 export const LoginSignup = () => {
   const [state, setState]=useState("Login");
+  const [formData, setFormData]=useState({
+    username:"",
+    email:"",
+    password:""
+  })
+
+  const changeHandler=(e)=>{
+    let name=e.target.name;
+    let value=e.target.value;
+    setFormData({
+      ...formData,
+      [name]:value
+    })
+  }
   const login=()=>{
-    console.log("login");
+    console.log("login", formData);
   }
   const signup=()=>{
-    console.log("signup");
+    console.log("signup", formData);
   }
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
         <h1>{state}</h1>
       <div className="loginsignup-fields">
-        {state==="Signup"? <input type='text' placeholder='Your Name'/> :<></>}
-        <input type='email' placeholder='Your email'/>
-        <input type='password' placeholder='Your password'/>
+        {state==="Signup"? <input type='text' name="username" placeholder='Your Name' value={formData.username} onChange={changeHandler}/> :<></>}
+        <input type='email' name='email' placeholder='Your email' value={formData.email} onChange={changeHandler}/>
+        <input type='password' name='password' placeholder='Your password' value={formData.password} onChange={changeHandler}/>
       </div>
       <button onClick={()=>{state==="Login"? login(): signup()}}>Continue</button>
       {state==="Signup"?
